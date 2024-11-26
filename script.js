f6c7610775aa48e192168020991240e0d4bc4798
shahzaib260 <151397723+shahzaib260@users.noreply.github.com>
2024-11-26 16:59:48 +0500

    Files Added
    Files Added

---
+++ script.js
@@ -0,0 +1,13 @@
+const name1 = document.querySelector("")
+
+const name2 = document.querySelector("")
+
+const name3 = document.querySelector("")
+
+const name4 = document.querySelector("")
+
+const name5 = document.querySelector("")
+
+const name6 = document.querySelector("")
+
+const name7 = document.querySelector("")
